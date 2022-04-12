e0d2de2b028cb9f291248e06a1e2a896648aa0f3
derimulia <derimulia95@gmail.com>
2022-04-13 00:21:37 +0700

    git add .

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const usersRouter = require("./app/api/v1/users/router");
 const authRouter = require("./app/api/v1/auth/router");
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 // app.use("/", (req, res) => {
 //   res.json({ message: "welcom to api" });
